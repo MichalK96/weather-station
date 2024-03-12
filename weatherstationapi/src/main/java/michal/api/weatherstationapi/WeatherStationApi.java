bdc3fef7d40a99a363b64915f4e08bca9dbb624d
MichalK96 <miniocha1206@wp.pl>
2024-03-12 15:13:24 +0100

    New: Repo initialize

---
+++ weatherstationapi/src/main/java/michal/api/weatherstationapi/WeatherStationApi.java
@@ -0,0 +1,13 @@
+package michal.api.weatherstationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStationApi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStationApi.class, args);
+	}
+
+}
---
+++ weatherstationapi/src/test/java/michal/api/weatherstationapi/WeatherstationapiApplicationTests.java
@@ -0,0 +1,13 @@
+package michal.api.weatherstationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherstationapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
